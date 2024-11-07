a60107b68a3e9a95a1bb6e881e06addac7ad3bf0
GennadiiMoroz <94042202+MAVIGEVA@users.noreply.github.com>
2024-11-07 09:40:20 +0500

    task number two, performed addition

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,19 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        var dogAdd = dog + 4;
-        var catAdd = cat + 4;
-        var paperAdd = paper + 4;
-        System.out.println(dogAdd);
-        System.out.println(catAdd);
-        System.out.println(paperAdd);
+
+        //var dogAdd = dog + 4;
+        //var catAdd = cat + 4;
+        //var paperAdd = paper + 4;
+        //System.out.println(dogAdd);
+        //System.out.println(catAdd);
+        //System.out.println(paperAdd);
+
+        var dogMinus = dog - 3.5;
+        var catMinus = cat - 1.6;
+        var paperMinus = paper - 7639;
+        System.out.println(dogMinus);
+        System.out.println(catMinus);
+        System.out.println(paperMinus);
     }
 }
